b0ca83dd4396d5c759cf63f2580a2fe8b055df1d
Davide Spada <dav.spada@outlook.com>
2024-06-26 18:30:48 +0200

    fix camera bindings and speeds

--- src/camera.js
+++ src/camera.js
@@ -7,31 +7,21 @@ export function initializeCamera(radius) {
   const zoomSpeed = 0.05;
   const cameraSpeedGamepad = 0.05;
   const zoomSpeedGamepad = 0.25;
-  let internalCamera = false; // Boolean flag to control the mode
-  const specificPosition = [-0.3218847370131798, 2.25, -3.9362626986652223]; // Example specific position
 
   let isDragging = false;
   let lastX = 0;
   let lastY = 0;
   let initialPinchDistance = null;
 
+
   function clamp(value, min, max) {
     return Math.max(min, Math.min(max, value));
   }
 
   function updateCamera() {
-    if (internalCamera) {
-      // cameraPosition[0] = cameraPosition[0];
-      cameraPosition[1] = specificPosition[1];
-      cameraPosition[2] = specificPosition[2];
-      cameraPosition = m4.xRotation(cameraPosition, 0, 0, radius);
-    } else {
-      cameraPosition[0] = Math.sin(cameraAngleRadians) * cameraRadius;
-      cameraPosition[2] = Math.cos(cameraAngleRadians) * cameraRadius;
-    }
-    cameraRadius = clamp(cameraRadius, -28, 28); // Clamp cameraRadius between -180 and 180
-    //debug for camera position
-    //console.log(cameraRadius);
+    cameraPosition[0] = Math.sin(cameraAngleRadians) * cameraRadius;
+    cameraPosition[2] = Math.cos(cameraAngleRadians) * cameraRadius;
+    cameraRadius = clamp(cameraRadius, -28, 28); // Clamp cameraRadius between -28 and 28
   }
 
   function onMouseDown(event) {
@@ -39,37 +29,30 @@ export function initializeCamera(radius) {
     lastX = event.clientX;
     lastY = event.clientY;
   }
-
   function onMouseUp() {
     isDragging = false;
   }
-
   function onMouseMove(event) {
     if (isDragging) {
       const deltaX = event.clientX - lastX;
       const deltaY = event.clientY - lastY;
       cameraAngleRadians -= deltaX * cameraSpeedMouse;
-      if (!internalCamera) {
-        cameraPosition[1] -= deltaY * zoomSpeed;
-      }
+      cameraPosition[1] -= deltaY * zoomSpeed;
       lastX = event.clientX;
       lastY = event.clientY;
       updateCamera();
     }
   }
-
   function onWheel(event) {
     cameraRadius += event.deltaY * zoomSpeed;
-    cameraRadius = clamp(cameraRadius, -180, 180); // Clamp cameraRadius between -180 and 180
+    cameraRadius = clamp(cameraRadius, -28, 28);
     updateCamera();
   }
-
   function getPinchDistance(touches) {
     const dx = touches[0].clientX - touches[1].clientX;
     const dy = touches[0].clientY - touches[1].clientY;
     return Math.sqrt(dx * dx + dy * dy);
   }
-
   function onTouchStart(event) {
     if (event.touches.length === 1) {
       isDragging = true;
@@ -80,15 +63,12 @@ export function initializeCamera(radius) {
       initialPinchDistance = getPinchDistance(event.touches);
     }
   }
-
   function onTouchMove(event) {
     if (isDragging && event.touches.length === 1) {
       const deltaX = event.touches[0].clientX - lastX;
       const deltaY = event.touches[0].clientY - lastY;
       cameraAngleRadians -= deltaX * cameraSpeedMouse;
-      if (!internalCamera) {
-        cameraPosition[1] -= deltaY * zoomSpeed;
-      }
+      cameraPosition[1] -= deltaY * zoomSpeed;
       lastX = event.touches[0].clientX;
       lastY = event.touches[0].clientY;
       updateCamera();
@@ -96,17 +76,15 @@ export function initializeCamera(radius) {
       const newPinchDistance = getPinchDistance(event.touches);
       const pinchDelta = newPinchDistance - initialPinchDistance;
       cameraRadius -= pinchDelta * (zoomSpeed / 100);
-      cameraRadius = clamp(cameraRadius, -180, 180); // Clamp cameraRadius between -180 and 180
+      cameraRadius = clamp(cameraRadius, -28, 28);
       initialPinchDistance = newPinchDistance;
       updateCamera();
     }
   }
-
   function onTouchEnd() {
     isDragging = false;
     initialPinchDistance = null;
   }
-
   function onKeyDown(event) {
     switch (event.key) {
       case 'ArrowUp':
@@ -122,60 +100,44 @@ export function initializeCamera(radius) {
         cameraAngleRadians += cameraSpeedKeyboard;
         break;
     }
-    cameraRadius = clamp(cameraRadius, -180, 180); // Clamp cameraRadius between -180 and 180
+    cameraRadius = clamp(cameraRadius, -28, 28);
     updateCamera();
   }
-
   function updateGamepad() {
     const gamepads = navigator.getGamepads();
     if (gamepads[0]) {
       const gp = gamepads[0];
-      const rightStickX = gp.axes[0];
-      const rightStickY = gp.axes[1];
-      // const rightStickX = gp.axes[2];
-      // const rightStickY = gp.axes[3];
+      const leftStickX = gp.axes[0];
+      const leftStickY = gp.axes[1];
+      const rightStickX = gp.axes[2];
+      const rightStickY = gp.axes[3];
       const leftTrigger = gp.buttons[6].value;
       const rightTrigger = gp.buttons[7].value;
 
       // Camera rotation
-      cameraAngleRadians += rightStickX * cameraSpeedGamepad;
-      if (!internalCamera) {
-        cameraPosition[1] -= rightStickY * cameraSpeedGamepad;
-      }
+      cameraAngleRadians += leftStickX * cameraSpeedGamepad;
+      cameraPosition[1] -= leftStickY * cameraSpeedGamepad;
 
       // Camera zoom
       cameraRadius -= (rightTrigger - leftTrigger) * zoomSpeedGamepad;
-      cameraRadius = clamp(cameraRadius, -180, 180); // Clamp cameraRadius between -180 and 180
-
+      cameraRadius = clamp(cameraRadius, -28, 28);
       updateCamera();
     }
-
     requestAnimationFrame(updateGamepad);
-  }
-
-  function setSpecificPosition() {
-    cameraPosition[0] = specificPosition[0];
-    cameraPosition[1] = specificPosition[1];
-    cameraPosition[2] = specificPosition[2];
-    cameraRadius = specificPosition[2];
-    updateCamera();
   }
 
   document.addEventListener('mousedown', onMouseDown);
   document.addEventListener('mouseup', onMouseUp);
-  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mousemove', onMouseMove); 
   document.addEventListener('wheel', onWheel);
   document.addEventListener('touchstart', onTouchStart);
   document.addEventListener('touchmove', onTouchMove);
   document.addEventListener('touchend', onTouchEnd);
   document.addEventListener('keydown', onKeyDown);
-
   requestAnimationFrame(updateGamepad);
-
   return {
     getCameraPosition: () => cameraPosition,
     getCameraAngleRadians: () => cameraAngleRadians,
     getCameraRadius: () => cameraRadius,
-    setInternalCamera: (value) => { internalCamera = value; if (internalCamera) setSpecificPosition(); }
   };
 }
